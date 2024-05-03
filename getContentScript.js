ec4ac901e1ed23d87eb72ff338bbea2832957288
eddieoz <osoriojr@gmail.com>
2024-05-03 12:07:20 +0300

    add get selected txt on pdfs

--- getContentScript.js
+++ getContentScript.js
@@ -13,17 +13,32 @@
     try {
         const loadingTask = pdfjsLib.getDocument(url);
         const pdfDoc = await loadingTask.promise;
-        let fullText = '';
 
-        for (let pageNum = 1; pageNum <= pdfDoc.numPages; pageNum++) {
-            const page = await pdfDoc.getPage(pageNum);
-            const textContent = await page.getTextContent();
-            const pageText = textContent.items.map(item => item.str).join(' ');
-            fullText += pageText + '\n';
+        // Get the current selection
+        const selection = window.getSelection();
+        console.log('Selected text: ',selection.toString())
+        if (selection.toString()) {
+            // If there's a selection, use that
+            chrome.runtime.sendMessage({
+                command: 'sendPdfContent',
+                content: selection.toString(),
+                language: localStorage.getItem('selectedLanguage')
+            });
+        } else {
+            // Otherwise, extract all text from the PDF
+            let fullText = '';
+            for (let pageNum = 1; pageNum <= pdfDoc.numPages; pageNum++) {
+                const page = await pdfDoc.getPage(pageNum);
+                const textContent = await page.getTextContent();
+                const pageText = textContent.items.map(item => item.str).join(' ');
+                fullText += pageText + '\n';
+            }
+            chrome.runtime.sendMessage({
+                command: 'sendPdfContent',
+                content: fullText,
+                language: localStorage.getItem('selectedLanguage')
+            });
         }
-
-        const selectedLanguage = localStorage.getItem('selectedLanguage');
-        chrome.runtime.sendMessage({command: 'sendPdfContent', content: fullText, language: selectedLanguage});
     } catch (err) {
         console.error('Error extracting PDF:', err);
     }
